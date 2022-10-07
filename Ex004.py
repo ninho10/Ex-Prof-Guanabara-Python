aa08298ac65954a784b552baa8764d06314d23a1
Paulo Roberto <paulo.trabalho10@gmail.com>
2022-10-07 18:19:28 -0300

    Create Ex004.py

---
+++ Ex004.py
@@ -0,0 +1,11 @@
+# De input em um nome, mostrando ele dentro de algumas função protas
+
+a = input('Digite algo : ')
+print(' o tipo primitivo desse valor e : ', type(a))
+print(' só tem espaço : ', a.isspace())
+print(' e um numero : ', a.isnumeric())
+print(' ele e alfabético : ', a.isalpha())
+print(' ele e alfanumérico : ', a.isalnum())
+print(' esta em MAIÚSCULA : ', a.isupper())
+print(' esta em minuscula : ', a.islower())
+print(' esta capitalizada : ', a.istitle())
