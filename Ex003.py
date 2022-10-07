2e514b56286bbfe53d6d45b0f3d39da204c836fa
Paulo Roberto <paulo.trabalho10@gmail.com>
2022-10-07 18:17:59 -0300

    Create Ex003.py

---
+++ Ex003.py
@@ -0,0 +1,6 @@
+# De input em 2 valores, de o valor da soma deles ....
+
+N1 = int(input('digite um valor : '))
+N2 = int(input('digite um valor : '))
+Soma = N1 + N2
+print(' O resultado de {} + {} = {} ' .format(N1, N2, Soma))
