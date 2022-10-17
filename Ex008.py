4334103e3895a79f7dae85c7d32ded6077d651a6
Paulo Roberto <paulo.trabalho10@gmail.com>
2022-10-17 14:06:56 -0300

    Create Ex008.py
    Digite 2 valor e mostre qual e o maior ou se e Igual.

---
+++ Ex008.py
@@ -0,0 +1,13 @@
+# Digite 2 valor e mostre qual e o maior ou se e Igual.
+
+
+N1 = int(input('Digite o primeiro numero : '))
+N2 = int(input('Digite o segundo numero : '))
+
+
+if N1 > N2:
+    print(' Primeiro numero maior ')
+elif N2 > N1:
+    print('Segundo numero maior ')
+else:
+    print('Valor igual')
